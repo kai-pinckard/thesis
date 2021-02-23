b2ce8580c1833dd9ba0fb3a9b09605d15e994b65
unknown <kai.pinckard@gmail.com>
2021-02-22 18:38:53 -0800

    improve translation script performance and accuracy. this was done by providing additional context for e1 and e2 translations by translating the json for a datapoint all at once instead of individually

--- translate_data.py
+++ translate_data.py
@@ -18,31 +18,56 @@ def reword_sentence(sent, lang):
     print(sent.text)
     return sent.text
 
+"""
+this works! Note that you need to remove all punctuation from the words otherwise it will
+crash by saying that the word is not in the vocabulary.
+from gensim.models import Word2Vec
+
+import gensim.downloader as api
+wv = api.load('word2vec-google-news-300')
+
+
+sente = "the baby cried in its crib"
+for word in sente.split():
+    print(word, "cradle")
+    print(wv.similarity(word, "cradle"))
+"""
+
 if __name__ == "__main__":
 
     import json
     with open(".\\classifier\\semeval2010task8\\semeval_datasetV2.json", "r") as f:
         dataset = json.load(f)
 
-    print(len(dataset))
-    dataset = dataset[:1]
-    print(dataset)
+    #print(len(dataset))
+    dataset = dataset[:2]
+    #print(dataset)
     reworded_data = []
     translation_language = "es"
 
+    # split the json file into chunks
+    data_chunks = []
+    data_chunk = "" 
+    chunk_num = 0
+    for i, data_point in enumerate(dataset):
+        # split the json data into chunks of size 50 to be below the 15,000 char limit
+        if i % 50 == 0 and i != 0:
+            data_chunks.append(data_chunk)
+            data_chunk = ""
+        data_point_str = json.dumps(data_point)
+        data_chunk += data_point_str + ",\n"
 
-    for d in dataset:
-        data_point = {}
-        data_point["sent"] = reword_sentence(d["sent"], translation_language)
-        data_point["e1_contents"] = reword_sentence(d["e1_contents"], translation_language)
-        data_point["e2_contents"] = reword_sentence(d["e2_contents"], translation_language)
-        data_point["is_active"] = d["is_active"]
-        data_point["relation_type"] = d["relation_type"]
-        data_point["translation_language"] = translation_language
-        reworded_data.append(data_point)
-    
-    print(reworded_data)
+    if data_chunk.endswith(","):
+        data_chunk = data_chunk[:-1]
+    data_chunks.append(data_chunk)
 
+    #print(data_chunks)
+    reworded_data = []
+    for chunk in data_chunks:
+        reworded_data.append(json.loads(reword_sentence(chunk, translation_language)))
+
+
+    #print(reworded_data)
     with open(".\\classifier\\semeval2010task8\\augmented.json", "w") as f:
         json.dump(reworded_data, f, indent=4)
 
